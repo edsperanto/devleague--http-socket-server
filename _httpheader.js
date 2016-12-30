f906731b61361e10bd9291c617e55463501ae882
Nick Lee <nicklee808@gmail.com>
2016-12-29 17:34:01 -1000

    switch cases setup

---
+++ _httpheader.js
@@ -0,0 +1,7 @@
+module.exports = `HTTP/1.1 200 OK
+Server: nginx/1.4.6 (Ubuntu)
+Date: HURRDURR
+Content-Type: text/html; charset=utf-8
+Content-Length: 40669
+Connection: keep-alive
+X-Powered-By: Express`
--- server.js
+++ server.js
@@ -5,13 +5,40 @@ const _helium = require('./_helium');
 const _hydrogen = require('./_hydrogen');
 const _index = require('./_index');
 const _styles = require('./_styles');
+const _httpheader = require('./_httpheader');
 const net = require('net');
 
 let server = net.createServer(function(socket) {
 	socket.setEncoding('utf8');
 	socket.on('data', (chunk) => {
-		console.log(chunk);
-    socket.write(_404);
+    let line = chunk.split('\n');
+    let request = line[0].split(' ');
+    console.log(request[0]);
+    if (request[0] === 'GET'){
+      switch (request[1]){
+        case '/':
+        case '/index.html':
+          socket.write(_httpheader);
+          socket.write(_index);
+          break;
+        case '/hydrogen.html':
+          socket.write(_httpheader);
+          socket.write(_hydrogen);
+          break;
+        case 'helium.html':
+          socket.write(_httpheader);
+          socket.write(_helium);
+          break;
+        case '404.html':
+          socket.write(_httpheader);
+          socket.write(_404);
+          break;
+        case '/css/styles.css':
+          socket.write(_httpheader);
+          socket.write(_styles);
+          break;
+      }
+    }
     socket.end();
     socket.destroy();
 	});
