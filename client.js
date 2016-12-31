f9893668da3f4ca5920c090e2f54615a8be6d92b
Nick Lee <nicklee808@gmail.com>
2016-12-30 20:53:23 -1000

    prepare for anal

--- client.js
+++ client.js
@@ -2,17 +2,24 @@
 
 const net = require('net');
 
+if (process.argv.length <= 2) {
+    console.log("Usage: client.js www.devleague.com");
+    process.exit(-1);
+}
+ 
+let param = process.argv[2];
+let hostname = param;
+let uri = '/';
+
 let client = net.createConnection(8080, 'localhost');
 client.setEncoding('utf8');
 client.on('connect', () => {
   console.log('connected');
   process.stdin.pipe(client);
-  client.write(`GET / HTTP/1.1
-Host: localhost:8080
+  client.write(`GET ${uri} HTTP/1.1
+Host: ${hostname}
 Content-Type: application/json
-Cache-Control: no-cache
-Postman-Token: 3072a901-9099-6bbb-e8c6-edde158f5c5a`);
-
+Cache-Control: no-cache`);
 });
 
 client.on('data', function (chunk) {
